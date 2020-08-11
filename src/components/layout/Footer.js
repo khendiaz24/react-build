d4f1a534b6a21efc981c635d3228badb783d9072
Khen Elvin Diaz <khendiaz@gmail.com>
2020-08-11 16:19:31 +0800

    add react-build to production

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  
+  return(
+    
+    <footer className="footer">
+      <div className="container text-center p-3">
+        <p>This is the footer</p>
+      </div>
+    </footer>
+  
+  )
+}
+
+export default Footer;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  
+  return (
+    
+    <header className="header">
+      <div className="container text-center p-3">
+        <p>This is header</p>
+      </div>
+    </header>
+  
+  )
+}
+
+export default Header;
