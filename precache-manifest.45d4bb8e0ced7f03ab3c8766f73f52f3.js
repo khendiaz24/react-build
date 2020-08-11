8df526bf6fe2febce812299e9327604de2d1ffbc
Khen Elvin Diaz <khendiaz@gmail.com>
2020-08-11 16:28:47 +0800

    Updates

---
+++ precache-manifest.45d4bb8e0ced7f03ab3c8766f73f52f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a94c93471ebd650a41e6b2d8922e752",
+    "url": "/react-build/index.html"
+  },
+  {
+    "revision": "58794729a1e8dc795791",
+    "url": "/react-build/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6356a3169faec64da17d",
+    "url": "/react-build/static/css/main.f42fbee7.chunk.css"
+  },
+  {
+    "revision": "58794729a1e8dc795791",
+    "url": "/react-build/static/js/2.e9f06777.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-build/static/js/2.e9f06777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6356a3169faec64da17d",
+    "url": "/react-build/static/js/main.544a196c.chunk.js"
+  },
+  {
+    "revision": "cc469fe3cbed59ffee61",
+    "url": "/react-build/static/js/runtime-main.f10f69e3.js"
+  }
+]);
