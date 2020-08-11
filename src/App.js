806d0a57ed0535806024db0fb37ecb5a5d0b4c55
Khen Elvin Diaz <khendiaz@gmail.com>
2020-08-11 16:40:33 +0800

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <div className="App">
         <Header/>
         <main className="main">
+          <Route exact path="/" component={Home}/>
           <Route exact path="/react-build" component={Home}/>
         </main>
         <Footer/>
